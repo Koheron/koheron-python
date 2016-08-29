c2ac9671adfafa91817be6bfe0d895dd6215d2df
jeanminet <jean.minet@gmail.com>
2016-08-29 16:03:45 +0200

    add version to cli

--- koheron/koheron.py
+++ koheron/koheron.py
@@ -16,17 +16,29 @@ ConnectionError = requests.ConnectionError
 # CLI
 # --------------------------------------------
 
+class ConnectionType(object):
+    def __init__(self, host="", unixsock=""):
+        self.host = host
+        self.unixsock = unixsock
+
 @click.group()
 @click.option('--host', default='', help='Host ip address', envvar='HOST')
 @click.option('--unixsock', default='', help='Unix Socket path', envvar='UNIX_SOCK')
 @click.pass_context
 def cli(ctx, host, unixsock):
-    ctx.obj = KoheronClient(host=str(host), unixsock=unixsock)
+    if host != "" or unixsock != "":
+        ctx.obj = ConnectionType(host=str(host), unixsock=unixsock)
+
+@cli.command()
+def version():
+    from .version import __version__
+    click.echo(__version__)
 
 @cli.command()
 @click.pass_obj
 @click.argument('led_value', type=click.INT)
-def set_led(client, led_value):
+def set_led(conn_type, led_value):
+    client = KoheronClient(host=conn_type.host, unixsock=conn_type.unixsock)
     from .common import Common
     driver = Common(client)
     driver.set_led(led_value)
@@ -34,14 +46,16 @@ def set_led(client, led_value):
 
 @cli.command()
 @click.pass_obj
-def get_led(client):
+def get_led(conn_type):
+    client = KoheronClient(host=conn_type.host, unixsock=conn_type.unixsock)
     from .common import Common
     driver = Common(client)
     click.echo(driver.get_led())
 
 @cli.command()
 @click.pass_obj
-def init(client):
+def init(conn_type):
+    client = KoheronClient(host=conn_type.host, unixsock=conn_type.unixsock)
     from .common import Common
     driver = Common(client)
     driver.init()
