e89df21f89ef941a88bddccf15b6f93bb47d5753
tvanderbruggen <th.vanderbruggen@gmail.com>
2016-10-16 16:33:20 +0100

    Fix test name

--- koheron/test/tests.py
+++ koheron/test/tests.py
@@ -331,7 +331,7 @@ def test_rcv_string(tests):
     assert tests.rcv_std_string('Hello World')
 
 @pytest.mark.parametrize('tests', [tests, tests_unix])
-def test_rcv_string(tests):
+def test_rcv_string1(tests):
     assert tests.rcv_std_string1('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.')
 
 @pytest.mark.parametrize('tests', [tests, tests_unix])
